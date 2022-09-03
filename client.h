dfa8635a11f3b6b4243069b7c802d64448272da3
Zachary Taylor <ztaylor@ethz.ch>
2022-09-03 21:34:48 +0200

    Update client.h

--- client.h
+++ client.h
@@ -1,43 +1,24 @@
 #pragma once
 
-#include <netdb.h>
-#include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <algorithm>
-#include <array>
-#include <exception>
-#include <string>
+#include <memory>
 
-enum class Command { Sequence, FindWhite, Nudge };
-enum Color : int { Unknown = 0, Black, Blue, Green, Yellow, Red, White, Brown, Count };
+enum class Command { Sequence, FindWhite, Nudge, Ping };
+enum Color : int { Unknown = 0, Black, Blue, Green, Yellow, Red, White, Brown };
 
-class Ev3 {
- public:
-  Ev3();
-  ~Ev3();
-};
-
-class ColorHistogram {
- public:
-  void addColor(const Color& color);
-  void removeColor(const Color& color);
-  Color getMostCommonColor() const;
-
- private:
-  std::array<int, Color::Count> histogram_ = {};
-};
+constexpr float kTicksPerBlock = 47.0f;
 
 class ServerIO {
+ public:
   ServerIO(const std::string& serverName, const int portNumber);
   ~ServerIO();
 
-  bool readNextCommand(int& additionalCommandData);
+  bool readNextCommand();
+  Command getCurrentCommand() const;
   int getNudgeDistance() const;
+
+  void sendSequenceStart() const;
+  void sendColor(const Color& color, const int timesToSend) const;
+  void sendSequenceStop() const;
 
  private:
   Command currentCommand_;
@@ -49,8 +30,13 @@ class Conveyer {
  public:
   Conveyer();
 
+  void setSlow() const;
+  void setFast() const;
+
+  void stop() const;
   bool isStopped() const;
-  int position() const;
+
+  int getPosition() const;
   void moveBy(const int distanceToMove) const;
 
  private:
@@ -65,4 +51,33 @@ class ColorSensor {
 
  private:
   uint8_t sensorAddress_;
+};
+
+class Ev3 {
+ public:
+  Ev3();
+  ~Ev3();
+
+  void sequence(const ServerIO& serverIO) const;
+  bool findWhite() const;
+  void moveConveyerBy(const int distanceToMove) const;
+
+ private:
+  std::unique_ptr<ColorSensor> colorSensor_;
+  std::unique_ptr<Conveyer> conveyer_;
+};
+
+class ColorDetector {
+ public:
+  ColorDetector(const int& consecutiveReadingThreshold);
+
+  void updateColorReading(const Color& color);
+
+  Color getCurrentColor() const;
+  bool colorAboveDetectionThreshold() const;
+
+ private:
+  const int consecutiveReadingThreshold_;
+  Color currentColor_ = Color::Unknown;
+  int consecutiveReadings_ = 0;
 };
